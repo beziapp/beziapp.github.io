335cdd8bddf6b73487cc66cc85bc2b345c11d42a
sijanec <sijanecantonluka@gmail.com>
2020-05-17 21:40:59 +0200

    changelog

--- dist/sw.js
+++ dist/sw.js
@@ -3,8 +3,8 @@
 
 
 // Change version to cause cache refresh
-const static_cache_name = "site-static-1.0.13-beta-519a2aa";
-// latest commit is 519a2aae0e5e8f797be3c75671ad3d1224a5697b
+const static_cache_name = "site-static-1.0.13-beta-7307222";
+// latest commit is 730722259f6f079c930f6fcce1c2f1e715d3df63
 // Got them with find . -not -path '*/\.*' | sed "s/.*/\"&\",/" | grep -v sw.js
 // sw.js NE SME BITI CACHAN, ker vsebuje verzijo!
 
