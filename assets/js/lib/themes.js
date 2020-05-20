cbef4f9e7f6b39469d9e6ac77b5a7237f69022ee
rstular <rok@stular.eu>
2020-05-20 19:37:34 +0200

    Night theme

--- assets/js/lib/themes.js
+++ assets/js/lib/themes.js
@@ -25,7 +25,7 @@ const THEME_COLOR_SCHEMES = {
         "color-divider": "rgba(224, 224, 224, 1)",
 
         "color-btn-flat": "rgba(52, 52, 52, 1)",
-        "color-btn-flat-disabled": "rgba(178, 178, 178, 1)",
+        "color-btn-flat-disabled": "rgba(178, 178, 178, 1)"
 
     },
     dark: {
@@ -54,7 +54,35 @@ const THEME_COLOR_SCHEMES = {
         "color-divider": "rgba(80, 80, 80, 1)",
 
         "color-btn-flat": "rgba(178, 178, 178, 1)",
-        "color-btn-flat-disabled": "rgba(52, 52, 52, 1)",
+        "color-btn-flat-disabled": "rgba(52, 52, 52, 1)"
+    },
+    black: {
+        "color-primary": "rgba(0, 79, 51, 1)",
+        "color-secondary": "rgba(0, 94, 61, 1)",
+        "color-accent": "rgba(28, 107, 79, 1)",
+        "color-primary-light": "rgba(0, 77, 2, 1)", // it's not really light anymore, is it?
+        "color-invalid": "rgba(138, 0, 0, 1)",
+        "color-primary-transparent": "rgba(0, 79, 51, 0.3)",
+
+        "background-color": "rgba(0, 0, 0, 1)",
+        "background-accent": "rgba(0, 38, 18, 0.2)",
+        "background-accent-1": "rgb(20, 33, 25, 1)",
+
+        "color-header-bold": "rgba(255, 255, 255, 1)",
+        "color-header-span": "rgba(255, 255, 255, 1)",
+
+        "color-text": "rgba(189, 189, 189, 1)",
+        "color-text-light": "rgba(160, 160, 160, 1)",
+        "color-label-text": "rgba(158, 158, 158, 1)",
+        "color-table-hover": "rgba(43, 43, 43, 0.5)",
+
+        "color-sidenav-background": "rgba(0, 0, 0, 1)",
+        "color-sidenav-text": "rgba(189, 189, 189, 1)",
+        "color-sidenav-header": "rgba(189, 189, 189, 0.54)",
+        "color-divider": "rgba(50, 50, 50, 1)",
+
+        "color-btn-flat": "rgba(178, 178, 178, 1)",
+        "color-btn-flat-disabled": "rgba(52, 52, 52, 1)"
     }
 }
 
